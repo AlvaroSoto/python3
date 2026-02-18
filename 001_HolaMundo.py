0b47224403740e7b87222d6db12d40a8cce2c23f
Tablet <alvaro.sg.design@gmail.com>
2026-02-17 21:59:38 -0600

    primer programa

---
+++ 001_HolaMundo.py
@@ -0,0 +1,19 @@
+# ============================================
+# MI PRIMER PROGRAMA EN PYTHON 🐍
+# ============================================
+
+# 1. IMPRIMIR EN PANTALLA
+print("¡Hola Mundo!")
+
+# 2. UNA VARIABLE (guarda un dato)
+nombre = "Ana"
+
+# 3. INPUT (pedirle algo al usuario)
+nombre = input("¿Cómo te llamas? ")
+
+# 4. PRINT con la variable
+print("¡Hola,", nombre, "!")
+
+# 5. EVAL (convierte texto a número para operar)
+edad = eval(input("¿Cuántos años tienes? "))
+print("En 10 años tendrás", edad + 10, "años")
